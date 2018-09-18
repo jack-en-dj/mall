47c601bf8f29090f383f76c258ffdb2543fbc72b
jack-en-dj <1752257382@qq.com>
2018-09-18 11:13:26 +0800

    test5

--- src/main/java/com/mmall/service/impl/FileServiceimpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceimpl.java
@@ -47,11 +47,10 @@ public class FileServiceimpl implements IFileService{
             将targetFile上传到我们的FTP服务器上
             文件上传成功，上传成功删除upload下载的文件
              */
-            System.out.println(targetFile+"失败了");
-            InputStream files =new FileInputStream(targetFile);
-            System.out.println(Lists.newArrayList(files));
             System.out.println(targetFile.isFile()+"是否为文件对象");
-            FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+            if (targetFile!=null){
+                FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+            }
             targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常",e);
